005b848bdabfd946c6ee02358b2561d19e887166
Oskar TRBRY Lindqvist <oskar@tranberry.se>
2021-05-04 21:52:40 +0200

    re-added data from original, some proofing done

--- store.js
+++ store.js
@@ -1,19 +1,104 @@
 // Var voor strings kinky
 export const QuoteText = [
-  'Dear :Cycy:, :insertpluralnoun: are :insertcolor:, :insertpluralnoun2:' +
-  ' are blue, you love me and I love :insertBluRhyme:!<br>your :Gwen:.',
+  // TODO: fixi fix this editing and proofing
+  `There are many :insertadjective: ways to choose what to read. <br>
+  First, you should ask for recommendations perhaps from :Cycy: and
+  :insertpluralnoun:. <br><br>
 
-  'Please excuse :Cycy: who is far too :insertadjective: to attend ' +
-    ' :insertnoun: class.<br> Signed by :Gwen:',
+  Whatever you do, don't ask :Gwen: whom only reads :insertadjective2: books
+  with :insertanimal:s on the cover!<br><br>
 
-  ':Cycy: is sick with :insertIllness:. Drink more :insertfluid: ' +
-    'and take :insertsubstance: as needed.<br> - Dr.:Gwen:',
+  If your friends and family are no help, try checking out the
+  :insertnoun2: Review in <em>The :insertcity: Times</em>.<br><br>
 
-  ':Cycy: is authorized to be at the :insertplace: instead of :insertnoun: ' +
-    ' class.<br> Signed by :Gwen:',
+  If the :insertpluralnoun2: featured there are too :insertadjective3: for your
+  taste, try something a little more low-:insertbodypart:, like :Cycy: would do.
 
-  ':Gwen: and :Cycy: are too :insertadjective: for :insertnoun: class. ' +
-  'Instead they will be attending the :insertevent:.',
+  You could also choose the :insertadjective4:-fashioned way. Head to
+  your local library or :insertplace: and browse the shelves until something
+  catches your :insertbodypart2:.<br><br>
+
+  Or, you could save yourself a whole lot of
+  :insertadjective5: trouble and log on :insertwebsite:, the :insertadjective6:
+  new website to :insertverb: for books! With all the time you will save not
+  having to search for :insertpluralnoun3: you can read :insertnumber:
+  more books!`,
+
+
+  `After hiding the :insertnoun: in :Cycy:s :insertbodypart:
+  for :insertnumber: years, :Gwen: grew :insertadjective: and tried to
+  sell it to a/an :insertanimal: in :insertcity:, but was caught.`,
+
+
+  `:Cycy: was walking in the park one day, humming to themselves.
+  The day was bright, the sun was shining, and the :insertanimal:s was singing.
+  <br><br> Suddenly, a :insertanimal2: ran by.
+  <br><br> Run! Run! the :insertanimal2: cried. :Gwen: is coming!
+  And :Gwen: is really angry about the :insertevent:!`,
+
+
+  // TODO: fixi fix this editing and proofing
+  `Dear :Cycy:,<br> :Gwen: is having a :inserttheme: party!
+   It is going to be at the :insertplace: on :insertweekday:.
+   Please make sure to show up, or else you will be required to
+   :insertverb: a/an :insertanimal: with your :insertbodypart:.`,
+
+
+  `Twinkle, twinkle, :insertadjective: :Cycy:. <br>
+  How I :insertverb: what you :insertverb2:.<br>
+  Up above the :insertnoun: so :insertadjective2:.<br>
+  Like a :insertnoun2: in the :insertnoun3:.<br>
+  Twinkle, twinkle, :insertadjective: :Cycy:.<br><i>
+  How I :insertverb: what you :insertverb2:.</i><br>
+  Yours :Gwen:!`,
+
+
+  `A :insertadjective: fairy princess movie is coming out soon!
+  It will feature Snow-:Cycy: and the :insertnumber: dwarfs.<br><br>
+
+  Synopsis:<br>
+  Snow-:Cycy: is a princess whose beauty challanges the evil queen. Hence
+  :Cycy: is forced to flee from :insertcity:-castle and hides in the
+  :insertplace:. There, she discovers the dwarfs playing with their
+  :insertpluralnoun:.<br>
+  But the vengeful queens search is not in vain she can casts a
+  :insertadjective2: spell.<br>
+  Catched in the :insertadjective2: spell until the :insertadjective3: :Gwen:
+  comes to rescue her, <strong>will wedding bells ring? come see!</strong>`,
+
+
+  `Here are some things to do at recess.
+   <ol>
+   <li> Start a game of touch :insertbodypart:-ball with :Cycy:.</li>
+   <li> Put a :insertnoun: in :Gwen:s lunch bag.</li>
+   <li> Start a :insertvegetable: fight in the school
+    :insertadjective:room against :Cycy:.</li>
+   <li> Choose sides and have a :insertverb:ball
+   tournament versus :Gwen:s team.</li>
+   <li> Demand more :insertpluralnoun: and shorter
+   :insertadjective2: classes with :Gwen:.</li>
+   <li> Choose :Cycy: to be <strong>it</strong>.</li>
+   </ol>`,
+
+
+  `Dear :Cycy:, :insertpluralnoun: are :insertcolor:, :insertpluralnoun2:` +
+  ` are blue, you love me and I love :insertBluRhyme:!<br>your :Gwen:.`,
+
+
+  `Please excuse :Cycy: who is far too :insertadjective: to attend ` +
+  ` :insertnoun: class.<br> Signed by :Gwen:`,
+
+
+  `:Cycy: is sick with :insertIllness:. Drink more :insertfluid: ` +
+  `and take :insertsubstance: as needed.<br> - Dr.:Gwen:`,
+
+
+  `:Cycy: is authorized to be at the :insertplace: instead of :insertnoun: ` +
+  ` class.<br> Signed by :Gwen:`,
+
+
+  `:Gwen: and :Cycy: are too :insertadjective: for :insertnoun: class. ` +
+  `Instead they will be attending the :insertevent:.`,
 ];
 
 // :insertadjective: + 2,3,4,5
@@ -25,7 +110,7 @@ export const adjective = [
 
 // :insertnoun: + 2,3
 export const noun = [
-  'array', 'badger', 'bracket', 'Harlowe', 'Sugarcube', 'disadvantage',
+  'array', 'badger', 'bracket', 'Harlowe', 'Sugarcube', 'reindoctrination',
   'Twine', 'interface', 'knitting', 'hoarding', 'mayonnaise', 'octopus', 'pike',
   'rail', 'scissors', 'cake', 'gaming', 'turkey', 'work',
 ];
@@ -85,7 +170,7 @@ export const eventthing = [
 
 // :insertweekday:
 export const weekday = [
-  'Monday', 'Teusday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday',
+  'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday',
 ];
 
 // :inserttheme:
@@ -151,5 +236,5 @@ export const illness = [
 // twine discord server usual suspects
 export const suspects = [
   'Cycy', 'Gwen', 'Redsea', 'Lychgate', 'Jay', 'TRBRY', 'Chapel', 'Greyelf',
-  'the Mad Exile', 'Somnium',
+  'TME', 'Somnium',
 ];
