afc509d07931295aae60fbdfd42f2432b364bcd5
Oskar TRBRY Lindqvist <oskar@tranberry.se>
2021-05-04 18:58:09 +0200

    refactor - init

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # SillySpooder
-To generate silly inside joxx 
-quick link: https://sjoerdhekking.github.io/SillySpooder/
+
+To generate silly inside joxx
+quick link: <https://sjoerdhekking.github.io/SillySpooder/>
---
+++ store.js
@@ -0,0 +1,177 @@
+// Var voor strings kinky
+export const QuoteText = [
+  'Dear :Cycy:, :insertpluralnoun: are :insertcolor:, :insertpluralnoun2:' +
+  ' are blue, you love me and I love :insertBluRhyme:!<br>your :Gwen:.',
+
+  'Please excuse :Cycy: who is far too :insertadjective: to attend ' +
+    ' :insertnoun: class.<br> Signed by :Gwen:',
+
+  ':Cycy: is sick with the :insertbodypart: flu. Drink more :insertfluid: ' +
+    'and take :insertsubstance: as needed.<br> Yours truly, :Gwen:',
+
+  ':Cycy: is authorized to be at the :insertplace: instead of :insertnoun: ' +
+    ' class.<br> Signed by :Gwen:',
+
+  ':Cycy: is too cool for :insertnoun: class. Instead they will be ' +
+    ' attending the :insertevent:',
+];
+
+// :insertadjective: + 2,3,4,5
+export const adjective = [
+  'aback', 'caring', 'eager', 'equal', 'flagrant', 'friendly', 'glossy',
+  'halting', 'helpless', 'ill', 'irate', 'lavish', 'madly', 'military',
+  'nebulous', 'obese', 'opposite', 'penitent', 'pricey', 'quirky',
+  'rhetorical', 'scandalous', 'sincere', 'spotless', 'tangy', 'thundering',
+  'unhealthy', 'waiting',
+];
+
+// :insertnoun: + 2,3
+export const noun = [
+  'accelerator', 'badger', 'bracket', 'character', 'country', 'disadvantage',
+  'fahrenheit', 'german', 'horn', 'knot', 'mayonnaise', 'octopus', 'pike',
+  'rail', 'scissors', 'sound', 'sycamore', 'turkey', 'work',
+];
+
+// :insertcity:
+export const city = [
+  'Aberdeen', 'Prune', 'Berlin', 'Burlington', 'Malmö', 'Danbury',
+  'Elk Grove', 'Aarhus', 'GreenBay', 'Glasgow', 'Solingen',
+  'Lakeland', 'Valencia', 'Miami', 'Naples', 'Marseille', 'Orlando',
+  'Pomona', 'Reading', 'Saint Petersburg', 'Milano', 'Springdale',
+  'Miskolc', 'Waco', 'Rotterdam',
+];
+
+// :insertverb:
+export const verb = [
+  'accept', 'balance', 'bubble', 'comb', 'damage', 'drown', 'fence', 'grip',
+  'improve', 'label', 'mend', 'owe', 'post', 'reach', 'rob', 'shiver', 'spot',
+  'suppose', 'treat', 'water',
+];
+
+// :insertanimal: + 2
+export const animal = [
+  'Aardvark', 'Armadillo', 'Bee', 'Cassowary', 'Chinchilla', 'Crab',
+  'Dogfish', 'Eagle', 'Ferret', 'Gazelle', 'Goose', 'Hare', 'Human',
+  'Jellyfish', 'Lark', 'Louse', 'Meerkat', 'Mule', 'Ostrich', 'Pelican',
+  'Quail', 'Raven', 'Sand Dollar', 'Shrew', 'Squirrel', 'Tarsier', 'Vulture',
+  'Wolverine',
+];
+
+// :insertpluralnoun: + 2,3
+export const pluralNoun = [
+  'aardvarks ', 'benches ', 'calculators ', 'coals ', 'diamonds ', 'flutes ',
+  'hats ', 'kayaks ', 'mascaras ', 'otters ', 'pockets ', 'sails ', 'sleds ',
+  'streams ', 'tortoises ', 'whips ',
+];
+
+// :insertfluid:
+export const fluid = [
+  'wator', 'water', 'beer', 'wine', 'mountain dew', 'coffee', 'thee', 'cola',
+  'fanta', 'sprite', 'whiskey', 'moonshine', 'milk', 'chocolate milk', 'blood',
+];
+
+// :insertsubstance:
+export const substance = [
+  'tin', 'sulfer', 'copper', 'diamond', 'sugar', 'soda', 'daimond', 'jello',
+  'flour', 'honey', 'sand', 'salt',
+];
+// :insertevent:
+export const eventthing = [
+  'twine jam', 'skribbl.io tournament', 'dungeon night at Som his place',
+  'voice channel party', 'sugarcube livestream',
+  'slumber night at :Gwen:s place', 'coding night', 'bulli :Gwen: day',
+  'drawing course with berry', 'game night with redsea', 'bloodsuck day',
+  'nosleep vamp party',
+];
+
+// :insertweekday:
+export const weekday = [
+  'Monday', 'Teusday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday',
+];
+
+// :inserttheme:
+export const theme = [
+  'Harry Potter magic', 'Hollywood Glamour', 'pink flamingo delights',
+  'jungle', 'bright and glowing', 'retro', 'sweet snow cone', 'carnival',
+  'luau time', 'donut', 'sock hop', 'circus galore', 'popsicle',
+  '1001 arabian nights', 'ice cream', 'margarita', 'bring back the eighties',
+  'watermelon', 'taco', 'fortnite', 'coding', 'slumber', 'twinery',
+];
+
+// what?
+export const veggie = [
+  'fake fruit', 'apple', 'pear', 'berry', 'banana', 'watermelon', 'tomato',
+  'mushroom', 'orange', 'mango', 'kiwi',
+];
+
+// :insertbodypart: + 2
+export const bodyPart = [
+  'eye', 'leg', 'chest', 'finger', 'belly', 'back', 'stomach', 'foot', 'nose',
+  'chin', 'hair', 'neck', 'knee', 'heel', 'cheek', 'tongue', 'lung', 'buttock',
+];
+
+// :insertplace:
+export const place = [
+  'Twine Server', 'discord', 'coffee shop', 'submarine', 'Somniums Inn',
+  'Sugarcube Clubhouse', 'The Lost Harlowe', 'Snowman peak', 'Chapbook Lake',
+  'Scary frinds home',
+];
+
+// :insertnumber
+export const number = [
+  'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+  'ten', 'eleven', 'ninety-six',
+];
+
+// :insertwebsite:
+export const website = [
+  'www.twinery.org', 'www.twinelab.net', 'www.discord.com',
+  'wwww.motoslave.com', 'www.github.com',
+];
+
+// :insertcolor:
+export const colors = [
+  'blue', 'black', 'green',
+];
+
+// :insetBluRhyme:
+export const rhymeBlu = [
+  'you', 'Drew', 'brew', 'EU', 'loo', 'roux', 'to', 'who', 'few', 'dew', 'glue',
+  'flu', 'zoo', 'boo', 'goo', 'mew', 'pooh', 'poo', 'gnu', 'anew', 'taboo',
+  'tattoo', 'bamboo', 'canoe', 'undo', 'sinew', 'fondue', 'kazoo', 'revenue',
+  'residue', 'caribou', 'shampoo', 'marabou', 'peekaboo', 'superglue',
+  'barbecue', 'kangaroo', 'cockatoo', 'honeydew',
+];
+
+/**
+ * Set highest number + no decimal.
+ * @param {number} max - highest number to generate
+ * @return {number} - random number less than or equal to <max>
+ */
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+/**
+ * Builds an array with random numbers in a pattern where the acending number
+ * gets subtracted from the total of previous numbers.
+ * @param {number} total highets number in array to build pattern from
+ * @param {number} count amount of numbers in array
+ * @return {number} gives an array o pattern nombahs.
+ */
+function splitRandom(total, count) {
+  const breaks = [0, total];
+  for (let i = 0; i < count - 1; i++) {
+    breaks.push(getRandomInt(total));
+  }
+  breaks.sort((a, b) => a - b);
+  console.log(breaks);
+  const values = [];
+  for (let i = 1; i < count + 1; i++) {
+    values.push(breaks[i] - breaks[i-1]);
+  }
+  return values;
+}
+
+
+console.log(splitRandom(20, 6));
